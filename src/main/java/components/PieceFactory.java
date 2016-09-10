659132f6f1510c4e66a2a907e2e9fb742dcaffca
SAURABH JAIN <saurabhjn76@gmail.com>
2016-09-10 15:28:26 +0530

    Update PieceFactory.java

--- src/main/java/components/PieceFactory.java
+++ src/main/java/components/PieceFactory.java
@@ -4,17 +4,17 @@ public class PieceFactory {
 	public Piece createPiece(PieceType pType,int x,int y,Color colr) {
 		switch(pType) {
 			case PieceType.PAWN: 	return new Pawn(x,y,colr);
-					   				break;
+					   	break;
 			case PieceType.CAMEL: 	return new Bishop(x,y,colr);
-									break;
+						break;
 			case PieceType.KING: 	return new King(x,y,colr);
-					   				break;
+					   	break;
 			case PieceType.QUEEN: 	return new Queen(x,y,colr);
-									break;
+						break;
 			case PieceType.HORSE: 	return new Knight(x,y,colr);
-									break;
+						break;
 			case PieceType.ELEPHANT:return new Rook(x,y,colr);
-						   			break;
+						break;
 			default: return null;
 		}
 	}
