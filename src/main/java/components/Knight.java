099d9bea6a469ed8ea62047d11f4f407675e2600
DrKay1996 <arti2k0103@gmail.com>
2016-09-12 15:42:11 +0530

    Added some temporary functions Location-Piece related functions in Board

--- src/main/java/components/Knight.java
+++ src/main/java/components/Knight.java
@@ -5,17 +5,44 @@ public class Knight extends Piece{
 
 	public Knight(int x, int y, Color colr){
 		super(x, y, colr);
+		Board.setLocPiece(initLoc,colr);
 	}
 	
 	@Override
-	public boolean validMove(Location destination){
-		//TODO:
-		return true;
+	public boolean validMove(Location destination){int x = curLoc.getX();
+		int y = curLoc.getY();
+		if( x == destination.getX() && y == destination.getY() ){
+			System.out.println("Cannot move from Current Location to Current Location.");
+			return false;
+		}
+
+		//Considering upper right corner of the Matrix as [0[0]
+		if( x+2 == destination.getX() || x-2 == destination.getX() ){
+			if( y-1 == destination.getY() || y+1 == destination.getY() ){
+				if(color == Board.getLocPiece(destination))
+					return false;
+				move(destination);
+				return true;
+			}
+			else 
+				return false;
+		}
+		else if( y+2 == destination.getY() || y-2 == destination.getY() ){
+			if( x-1 == destination.getX() || x+1 == destination.getX() ){
+				if(color == Board.getLocPiece(destination))
+					return false;
+				move(destination);
+				return true;
+			}
+			else
+				return false;
+		}
+		else 
+			return false;
 	}
 
 	@Override
 	public void move(Location validDest){
-		//TODO:
-		return ;
+		Board.setLocPiece(validDest,color);
 	}
 }
--- src/main/java/components/Rook.java
+++ src/main/java/components/Rook.java
@@ -5,17 +5,88 @@ public class Rook extends Piece{
 
 	public Rook(int x, int y, Color colr){
 		super(x, y, colr);
+		Board.setLocPiece(initLoc,colr);
 	}
 	
 	@Override
 	public boolean validMove(Location destination){
-		//TODO:
-		return true;
+		int x = curLoc.getX();
+		int y = curLoc.getY();
+		int i;
+		if(x == destination.getX() && y == destination.getY()){
+			System.out.println("Cannot move from Current Location to Current Location.");
+			return false;
+		}
+
+		//detecting if there is any obstacle in movement path
+		if ( x == destination.getX()){
+			// move horizontally
+
+			if ( y < destination.getY()){
+				//moving right
+				for(i = y+1 ; i < destination.getY(); i++){
+					Location tmpDest = new Location(x,i);
+					if(Board.getLocPiece(tmpDest) != null)
+						return false;
+				}
+				if(color == Board.getLocPiece(destination))
+					return false;
+				move(destination);
+				return true;
+			}
+
+			else{
+				//moving left
+				for(i = y-1 ; i> destination.getY(); --i){
+					Location tmpDest = new Location(x,i);
+					if( Board.getLocPiece(tmpDest) != null)
+						return false;
+				}
+				if(color == Board.getLocPiece(destination))
+					return false;
+				move(destination);
+				return true;
+			}
+		}
+
+		else if ( y == destination.getY()){
+
+			//move vertically
+
+			if ( x < destination.getX()){
+				//moving up
+				for(i = x+1 ; i < destination.getX(); i++){
+					Location tmpDest = new Location(i,y);
+					if(Board.getLocPiece(tmpDest) != null)
+						return false;
+				}
+				if(color == Board.getLocPiece(destination))
+					return false;
+				move(destination);
+				return true;
+			}
+
+			else{
+				//moving down
+				for(i = x-1 ; i> destination.getX(); --i){
+					Location tmpDest = new Location(i,y);
+					if( Board.getLocPiece(tmpDest) != null)
+						return false;
+				}
+				if(color == Board.getLocPiece(destination))
+					return false;
+				move(destination);
+				return true;
+			}
+		}
+
+		else
+			return false;
+
 	}
 
 	@Override
 	public void move(Location validDest){
-		//TODO:
-		return ;
+		Board.setLocPiece(validDest,color);
 	}
 }
