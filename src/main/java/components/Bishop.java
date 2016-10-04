f67e5f703890611137a4a6b1403a0d161ad9f8a0
saurabhjn76 <saurabhjn76@gmail.com>
2016-10-05 00:17:50 +0530

    Finished with piece's move and valid move functions.

--- src/main/java/components/Bishop.java
+++ src/main/java/components/Bishop.java
@@ -5,17 +5,38 @@ public class Bishop extends Piece{
 
 	public Bishop(int x, int y, Color colr){
 		super(x, y, colr);
+		Board.setLocPiece(initLoc,colr);
 	}
 	
-	@Override
+	
 	public boolean validMove(Location destination){
-		//TODO:
-		return true;
+		int x = curLoc.getX();
+		int y = curLoc.getY();
+		
+		
+		if(x == destination.getX() && y == destination.getY()){
+			System.out.println("Cannot move from Current Location to Current Location.");
+			return false;
+		}
+
+		
+		if(Math.abs(x - destination.getX()) == Math.abs(y - destination.getY())){
+
+			//yet to check if there's a piece on the way.
+			if(color == Board.getLocPiece(destination))
+					return false;
+			else
+				return true;
+		}
+		else 
+			return false;
+		
+		
 	}
+
 
 	@Override
 	public void move(Location validDest){
-		//TODO:
-		return ;	
+		Board.setLocPiece(validDest,color);	
 	}
 }
